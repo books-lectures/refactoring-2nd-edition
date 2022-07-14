ae2751d2bd4b9dfaa997dc91d4b5bf3b096f361a
praconfi <praconfi@gmail.com>
2022-07-15 00:14:55 +0900

    initial code

---
+++ chapter 01/statement.js
@@ -0,0 +1,76 @@
+let invoice = [
+    {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }
+]
+
+let plays = {
+    "hamlet": {"name": "hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "othello", "type": "tragedy"}
+}
+
+
+// 공연료 청구서를 출력하는 코드
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `청구내역 (고객명: ${invoice[0].customer})\n`;
+    const format = new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2
+    }).format;
+    
+    for(let perf of invoice[0].performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if(perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience -30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if(perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`)
+        }
+
+        // 포인트를 적립한다
+        volumeCredits += Math.max(perf.audience - 30, 0)
+        // 희극관객 5명마다 추가 포인트를 제공한다
+        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // 청구 내역을 출력한다
+        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        totalAmount += thisAmount
+    }
+    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `적립포인트: ${volumeCredits}점\n`;
+    
+    console.log(result)
+    return result
+
+}
+
+statement(invoice, plays)
